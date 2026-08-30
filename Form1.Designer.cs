b0907e68eaa79e0b0fabd78da7253675a5308efa
Ayah alaqad <aya1alaqad@gmail.com>
2026-08-30 17:45:23 +0300

    inital code

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,11 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.topPanel = new System.Windows.Forms.Panel();
+            this.lblScore = new System.Windows.Forms.Label();
             this.pbApple = new System.Windows.Forms.PictureBox();
             this.pbRestart = new System.Windows.Forms.PictureBox();
             this.pbStop = new System.Windows.Forms.PictureBox();
-            this.lblScore = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.topPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbApple)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbRestart)).BeginInit();
@@ -49,15 +51,28 @@
             this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.topPanel.Location = new System.Drawing.Point(0, 0);
             this.topPanel.Name = "topPanel";
-            this.topPanel.Size = new System.Drawing.Size(960, 67);
+            this.topPanel.Size = new System.Drawing.Size(982, 68);
             this.topPanel.TabIndex = 0;
+            // 
+            // lblScore
+            // 
+            this.lblScore.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblScore.AutoSize = true;
+            this.lblScore.BackColor = System.Drawing.Color.Transparent;
+            this.lblScore.Font = new System.Drawing.Font("Goudy Stout", 19.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.ForeColor = System.Drawing.Color.White;
+            this.lblScore.Location = new System.Drawing.Point(95, 15);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(45, 44);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "0";
             // 
             // pbApple
             // 
             this.pbApple.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.pbApple.BackColor = System.Drawing.Color.Transparent;
             this.pbApple.Image = global::Snake_Game.Properties.Resources.apple;
-            this.pbApple.Location = new System.Drawing.Point(12, 0);
+            this.pbApple.Location = new System.Drawing.Point(12, 1);
             this.pbApple.Name = "pbApple";
             this.pbApple.Size = new System.Drawing.Size(77, 67);
             this.pbApple.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -69,7 +84,7 @@
             this.pbRestart.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.pbRestart.BackColor = System.Drawing.Color.Transparent;
             this.pbRestart.Image = global::Snake_Game.Properties.Resources.restart;
-            this.pbRestart.Location = new System.Drawing.Point(757, 3);
+            this.pbRestart.Location = new System.Drawing.Point(779, 4);
             this.pbRestart.Name = "pbRestart";
             this.pbRestart.Size = new System.Drawing.Size(89, 61);
             this.pbRestart.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -81,35 +96,28 @@
             this.pbStop.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.pbStop.BackColor = System.Drawing.Color.Transparent;
             this.pbStop.Image = global::Snake_Game.Properties.Resources.play;
-            this.pbStop.Location = new System.Drawing.Point(859, 3);
+            this.pbStop.Location = new System.Drawing.Point(881, 4);
             this.pbStop.Name = "pbStop";
             this.pbStop.Size = new System.Drawing.Size(89, 61);
             this.pbStop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbStop.TabIndex = 0;
             this.pbStop.TabStop = false;
             // 
-            // lblScore
+            // timer1
             // 
-            this.lblScore.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.lblScore.AutoSize = true;
-            this.lblScore.BackColor = System.Drawing.Color.Transparent;
-            this.lblScore.Font = new System.Drawing.Font("Goudy Stout", 19.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblScore.ForeColor = System.Drawing.Color.White;
-            this.lblScore.Location = new System.Drawing.Point(95, 14);
-            this.lblScore.Name = "lblScore";
-            this.lblScore.Size = new System.Drawing.Size(45, 44);
-            this.lblScore.TabIndex = 1;
-            this.lblScore.Text = "0";
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackgroundImage = global::Snake_Game.Properties.Resources.bg;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(960, 569);
+            this.ClientSize = new System.Drawing.Size(982, 553);
             this.Controls.Add(this.topPanel);
             this.Name = "Form1";
             this.Text = "Snake Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.topPanel.ResumeLayout(false);
             this.topPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbApple)).EndInit();
@@ -126,6 +134,7 @@
         private System.Windows.Forms.PictureBox pbRestart;
         private System.Windows.Forms.PictureBox pbStop;
         private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,113 @@ namespace Snake_Game
 {
     public partial class Form1 : Form
     {
+        enum enLosingCause { hitBoundries=1,hitItSelf=2}
+        clsSnake snake;
+        bool isStoped = false;
+        int foodSize = 40;
+        Point food;
+        int score;
         public Form1()
         {
             InitializeComponent();
+            this.DoubleBuffered = true;
+        }
+
+        private void resetGameToDefault()
+        {
+            isStoped = false;
+            food = new Point(400, 400);
+            snake = new clsSnake();
+            snake.body.Add(new Point(500, 500));
+            score = 0;
+
+            lblScore.Text =score.ToString();
+            pbStop.Enabled = true;
+            timer1.Start();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resetGameToDefault();
+          
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(Properties.Resources.apple,food.X,food.Y,foodSize,foodSize);
+
+            foreach(Point p in snake.body)
+            {
+                e.Graphics.FillRectangle(Brushes.Brown, p.X, p.Y, snake.partSize,snake.partSize);
+            }
+        }
+
+        private void changeFoodPosition()
+        {
+            Random r = new Random();
+           food.X= r.Next(20,this.ClientSize.Width-20);
+           food.Y= r.Next(20, this.ClientSize.Height-20);
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (isStoped)
+                return true;
+
+            switch(keyData)
+            {
+                case (Keys.Up):
+                    if (snake.direction != clsSnake.enDirection.down)
+                        snake.direction = clsSnake.enDirection.up;
+                    return true;
+                case Keys.Down:
+                    if (snake.direction != clsSnake.enDirection.up)
+                        snake.direction = clsSnake.enDirection.down;
+                    return true;
+                case Keys.Right:
+                    if (snake.direction != clsSnake.enDirection.left)
+                        snake.direction = clsSnake.enDirection.right;
+                    return true;
+                case Keys.Left:
+                    if (snake.direction != clsSnake.enDirection.right)
+                        snake.direction = clsSnake.enDirection.left;
+                    return true;
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+        private void playerLose(enLosingCause cause)
+        {
+            timer1.Stop();
+            pbStop.Enabled = false;       
+            string massage = (cause == enLosingCause.hitItSelf ? "Snake hit it self" : "Snake hit the wall");
+            MessageBox.Show(massage, "Game Over");
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            snake.move();
+            Invalidate();
+
+            if (snake.isSnakeBreakBoundries(0, this.ClientSize.Width, topPanel.ClientSize.Height, this.ClientSize.Height))
+            {
+                playerLose(enLosingCause.hitBoundries);
+                return;
+            }
+            if (snake.isThereCollission())
+            {
+                playerLose(enLosingCause.hitItSelf);
+                return;
+            }
+
+            if (snake.isFoodHitted(food, foodSize))
+            {
+                snake.addPart();
+                changeFoodPosition();
+                score += 1;
+                lblScore.Text = score.ToString();
+            }
+           
         }
     }
 }
